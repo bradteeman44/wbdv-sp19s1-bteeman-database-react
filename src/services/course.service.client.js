a4a3edc2614a2d1dc77313b26dfce8f6149f9fba
Brad Teeman <bradteeman@gmail.com>
2019-01-25 09:05:15 -0500

    Created the course services file

---
+++ src/services/course.service.client.js
@@ -0,0 +1,29 @@
+function CourseServiceClient() {
+    this.createCourse = createCourse;
+    this.findAllCourses = findAllCourses;
+    this.findCourseById = findCourseById;
+    this.updateCourse = updateCourse;
+    this.deleteCourse = deleteCourse;
+    this.url = '';
+    var self = this;
+    
+    function createCourse(course) {
+
+    }
+
+    function findAllCourses() {
+
+    }
+
+    function findCourseById(id) {
+
+    }
+
+    function updateCourse(id, course) {
+
+    }
+
+    function deleteCourse(id) {
+
+    }
+}
