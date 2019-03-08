45a161e3ce41e3dfdcc21c4e481961eda9b644fd
Brad Teeman <bradteeman@gmail.com>
2019-03-08 15:14:42 -0500

    create module service functions

--- src/services/ModuleService.js
+++ src/services/ModuleService.js
@@ -1,13 +1,14 @@
 class ModuleService {
     MODULE_API_URL = "localhost:8080/api/modules/";
+    COURSE_API_URL = "localhost:8080/api/courses/";
 
-    findModuleById = moduleId =>
-        fetch(this.MODULE_API_URL + moduleId)
+    findModuleById = mid =>
+        fetch(this.MODULE_API_URL + mid)
             .then(response => response.json()
             );
 
-    findAllModules = courseId =>
-        fetch(this.COURSE_API_URL + courseId + "/modules").then(response => response.json());
+    findAllModules = cid =>
+        fetch(this.COURSE_API_URL + cid + "/modules").then(response => response.json());
 
     addModule = (course, module) => {
         fetch(this.COURSE_API_URL + course.id + "/modules", {
@@ -19,23 +20,21 @@ class ModuleService {
         }).then(response => response.json());
     };
 
-    deleteModule = (course, deleteModule) => {
+    deleteModule = (deleteModule) => {
         console.log(deleteModule.id);
         fetch(this.MODULE_API_URL + deleteModule.id, {
             method: 'delete'
-        }).then(response => response.json());
+        }).then();
     }
 
-    updateModule = (course, module) => {
+    updateModule = (module) => {
         fetch(this.MODULE_API_URL + module.id, {
             method: 'put',
             body: JSON.stringify(module),
             headers: {
                 'content-type': 'application/json'
             }
-        }).then(function (response) {
-            return response.json();
-        })
+        }).then(response => response.json());
     };
 }
 export default ModuleService;
